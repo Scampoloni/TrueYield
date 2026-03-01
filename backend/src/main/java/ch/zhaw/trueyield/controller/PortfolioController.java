a15cbeca62ee0f990b83f220d6b441cac284adae
Scampoloni <scamplui@students.zhaw.ch>
2026-03-01 19:05:42 +0100

    Implemented GET portfolio by ID endpoint. Closes #7

--- backend/src/main/java/ch/zhaw/trueyield/controller/PortfolioController.java
+++ backend/src/main/java/ch/zhaw/trueyield/controller/PortfolioController.java
@@ -1,14 +1,17 @@
 package ch.zhaw.trueyield.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import ch.zhaw.trueyield.model.Portfolio;
 import ch.zhaw.trueyield.model.dto.PortfolioCreateDTO;
+import ch.zhaw.trueyield.repository.PortfolioRepository;
 import ch.zhaw.trueyield.service.PortfolioService;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,9 +22,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class PortfolioController {
 
     private final PortfolioService portfolioService;
+    private final PortfolioRepository portfolioRepository;
 
-    public PortfolioController(PortfolioService portfolioService) {
+    public PortfolioController(PortfolioService portfolioService, PortfolioRepository portfolioRepository) {
         this.portfolioService = portfolioService;
+        this.portfolioRepository = portfolioRepository;
     }
 
     @PostMapping
@@ -35,5 +40,14 @@ public class PortfolioController {
         List<Portfolio> portfolios = portfolioService.getAllPortfolios();
         return new ResponseEntity<>(portfolios, HttpStatus.OK);
     }
-}
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Portfolio> getPortfolioById(@PathVariable String id) {
+        Optional<Portfolio> optionalPortfolio = portfolioRepository.findById(id);
+        if (optionalPortfolio.isPresent()) {
+            return new ResponseEntity<>(optionalPortfolio.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
