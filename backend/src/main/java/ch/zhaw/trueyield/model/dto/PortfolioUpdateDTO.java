764f8ba96a79b6a023fa6b8f5e51b5b1d3523f36
Scampoloni <scamplui@students.zhaw.ch>
2026-03-01 19:31:00 +0100

    Implemented PUT endpoint to update portfolio. Closes #8

---
+++ backend/src/main/java/ch/zhaw/trueyield/model/dto/PortfolioUpdateDTO.java
@@ -0,0 +1,11 @@
+package ch.zhaw.trueyield.model.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
+public class PortfolioUpdateDTO {
+    private String name;
+    private String description;
+}
